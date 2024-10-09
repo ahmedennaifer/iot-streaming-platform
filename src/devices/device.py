54eaf0d2a10538b1106b6e932edc93bd6fa4e544
ahmedennaifer <ahmedennaifer00@gmail.com>
2024-10-09 23:11:01 +0200

    added diagram in readme

--- src/devices/device.py
+++ src/devices/device.py
@@ -12,10 +12,13 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 console_handler = logging.StreamHandler()
-console_handler.setLevel(logging.DEBUG) 
+console_handler.setLevel(logging.DEBUG)
 formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
 console_handler.setFormatter(formatter)
 logger.addHandler(console_handler)
+
+# TODO : organize classes into  files
+# TODO : check logging issue -> only info gets logged
 
 
 class StatusType(Enum):
