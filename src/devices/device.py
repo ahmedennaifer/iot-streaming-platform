51ca613a5262c14fbd74d39008c16e3b84ddebc4
ahmedennaifer <ahmedennaifer00@gmail.com>
2024-10-09 20:23:42 +0200

    housekeeping

--- src/devices/device.py
+++ src/devices/device.py
@@ -12,7 +12,7 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 console_handler = logging.StreamHandler()
-console_handler.setLevel(logging.DEBUG)  # Set the console handler to capture all logs
+console_handler.setLevel(logging.DEBUG) 
 formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
 console_handler.setFormatter(formatter)
 logger.addHandler(console_handler)
