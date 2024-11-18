f4eb1ebe69c5dacc4653a83ec5a40bd23dd9e307
ahmedennaifer <ahmedennaifer00@gmail.com>
2024-11-18 18:55:45 +0100

    added to db

--- src/main/scala/consumer/sparkConsumer.scala
+++ src/main/scala/consumer/sparkConsumer.scala
@@ -1,8 +1,6 @@
 package consumer
 
-
-
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{SparkSession, DataFrame}
 import org.apache.spark.sql.types._
 import org.apache.spark.sql.functions._
 
@@ -15,14 +13,18 @@ object SparkConsumer {
       .master("local[2]")
       .getOrCreate()
 
-    val schema = StructType(Array(
-      StructField("DeviceId", StringType, nullable = false),
-      StructField("DeviceModel", StringType, nullable = true),
-      StructField("Status", StringType, nullable = false),
-      StructField("CurrentReading", FloatType, nullable = false),
-      StructField("BatteryLevel", FloatType, nullable = false),
-      StructField("Location", FloatType, nullable = false)
-    ))
+    import spark.implicits._
+
+    val schema = StructType(
+      Array(
+        StructField("DeviceId", StringType, nullable = false),
+        StructField("DeviceModel", StringType, nullable = true),
+        StructField("Status", StringType, nullable = false),
+        StructField("CurrentReading", FloatType, nullable = false),
+        StructField("BatteryLevel", FloatType, nullable = false),
+        StructField("Location", FloatType, nullable = false)
+      )
+    )
 
     val df = spark.readStream
       .format("kafka")
@@ -32,15 +34,28 @@ object SparkConsumer {
       .load()
 
     val parsedDf = df
-      .selectExpr("CAST(value AS STRING) as json") 
-      .select(from_json(col("json"), schema).as("data")) 
-      .select("data.*") 
+      .selectExpr("CAST(value AS STRING) as json")
+      .select(from_json(col("json"), schema).as("data"))
+      .select("data.*")
 
+    def writeToDatabase(batchDf: DataFrame, batchId: Long): Unit = {
+
+      batchDf.show(truncate = false)
+
+      batchDf.write
+        .format("jdbc")
+        .option("url", "jdbc:postgresql://db:5435/iot")
+        .option("dbtable", "data")
+        .option("user", "nafra")
+        .option("password", "test")
+        .option("driver", "org.postgresql.Driver")
+        .mode("append")
+        .save()
+    }
 
     val query = parsedDf.writeStream
+      .foreachBatch(writeToDatabase _)
       .outputMode("append")
-      .format("console")
-      .option("truncate", "false")
       .start()
 
     query.awaitTermination()
